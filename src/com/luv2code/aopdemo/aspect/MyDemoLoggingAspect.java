16c128b75633d251eefcc61f8800ed2e319495d7
Dmitrij <seo.wolf.d@gmail.com>
2019-12-02 12:15:52 +0100

    AOP pointcut declaration for package

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,23 +2,27 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-//	add all related advises for logging
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {
+	}
 	
-//	@beforeAdvice
 	
-//	@Before("execution(public void add*())")
-//	@Before("execution(* *())")
-//	@Before("execution(* add*(com.luv2code.aopdemo.Account, ..))")
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
-		
 		System.out.println("\n======>>>> @before advice");
+	}
+	
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("======>>>> Perfoming analytics");
 	}
 	
 }
