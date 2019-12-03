adb8fcc523a94d59130ad8bf2ec798b33997baaf
Dmitrij <seo.wolf.d@gmail.com>
2019-12-03 12:59:26 +0100

    @AfterReturning example

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.luv2code.aopdemo.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -13,6 +16,37 @@ import com.luv2code.aopdemo.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+	
+//	add a new advice @AfterReturning
+	
+	@AfterReturning(pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+		public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+		
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n==========> executing @AfterReturning " + method);
+		
+		System.out.println("result is:" + result);
+		
+//		let's post-process the data .... let's modify it
+//		convert to uppercase
+		convertAccountNamesToUpperCase(result);
+		
+		
+	}
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+////	my advice
+//		for(Account tempAccount : result) {
+//			tempAccount.setName(tempAccount.getName().toUpperCase());
+//		}
+//		teacher advice
+		for(Account tempAccount : result) {
+			String theUpperName = tempAccount.getName().toUpperCase();
+			tempAccount.setName(theUpperName);
+		}
+	
+}
 
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package com.luv2code.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.luv2code.aopdemo.Account;
@@ -10,8 +13,22 @@ public class AccountDAO {
 	private String name;
 	private String serviceCode;
 	
-	public void addAccount(Account theAccount, boolean vipFlag) {
+	public List<Account> findAccounts(){
+		List<Account> myAccounts = new ArrayList<>();
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Platinum");
+		Account temp3 = new Account("Luca", "Gold");
 		
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+		
+		return myAccounts;
+	}
+	
+	
+	
+	public void addAccount(Account theAccount, boolean vipFlag) {
 		System.out.println(getClass() + "               DOING DB WORK: Adding an account");
 		
 	}
